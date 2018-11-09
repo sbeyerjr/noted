3ae0cae9b64743d10006a62df1ac0fa55e3699de
Steven Beyer <sbeyerjr@gmail.com>
2018-11-08 17:03:28 -0700

    added folders and app component

---
+++ src/components/app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function App(props) {
+    return (
+      <div>
+        
+        <main role="main">
+         <p>Test</p>
+        </main>
+      </div>
+    );
+  }
